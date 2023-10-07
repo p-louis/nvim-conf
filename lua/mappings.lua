795cf191418153e4896b5652b4e1e9fe51e95582
Patrick Louis <patrick.louis@link-time.com>
2023-10-07 08:01:13 +0200

    Moved some settings into individual files

---
+++ lua/mappings.lua
@@ -0,0 +1,27 @@
+-- [[ Basic Keymaps ]]
+
+vim.keymap.set('v', 'J', ':m >+1<CR>gv=gv', { desc = 'Move selection down' })
+vim.keymap.set('v', 'J', ':m <-2<CR>gv=gv', { desc = 'Move selection down' })
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Buffer motions
+vim.keymap.set('n', '<leader>.', '<cmd>bn<CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<tab>', '<cmd>bn<CR>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<S-tab>', '<cmd>bp<CR>', { desc = 'Previous Buffer' })
+vim.keymap.set('n', '<leader>bd', '<cmd>bd<CR>', { desc = 'Delete Buffer' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', '<leader>crf', vim.lsp.codelens.refresh, { desc = '[C]odelens [R]e[f]resh' })
+vim.keymap.set('n', '<leader>cr', vim.lsp.codelens.run, { desc = '[C]odelens [R]un' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]odelens [R]un' })
