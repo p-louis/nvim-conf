d68d49655b798cd59c927b4954dbb6b5ba9d2d08
Patrick Louis <patrick.louis@linked-planet.com>
2024-12-05 11:31:02 +0100

    ADD fugitive

--- lua/Fuzzel/init.lua
+++ lua/Fuzzel/init.lua
@@ -83,7 +83,7 @@ vim.keymap.set('n', '<leader>gr', ':r !tr -dc a-z0-9 < /dev/urandom | head -c 36
 { desc = "Generate Random UID" })
 
 -- Colorscheme
-vim.cmd('colorscheme catppuccin')
+vim.cmd('colorscheme catppuccin-frappe')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
 
