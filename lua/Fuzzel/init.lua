288e772eba3b662b7702ccff0a3fe264b08268d3
Patrick Louis <patrick.louis@linked-planet.com>
2024-12-05 11:49:30 +0100

    CHG set colorscheme in init

--- lua/Fuzzel/init.lua
+++ lua/Fuzzel/init.lua
@@ -134,7 +134,7 @@ require('catppuccin').setup({
     treesitter = true,
   },
 })
-
+vim.cmd('colorscheme catppuccin')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
