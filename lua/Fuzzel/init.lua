512b694b36d702b9b01204c95bb0748ba57f5c2e
Patrick Louis <patrick.louis@linked-planet.com>
2024-12-05 19:21:06 +0100

    CHG restructure config

--- lua/Fuzzel/init.lua
+++ lua/Fuzzel/init.lua
@@ -5,58 +5,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require("todo-comments").setup()
-
-require("lualine").setup {
-options = {
-    icons_enabled = false,
-    theme = 'catppuccin',
-    component_separators = { left = '', right = '' },
-    section_separators = {
-      left = '',
-      right = '',
-    }
-  },
-}
-
-require("gitsigns").setup {
-  signs = {
-    add = { text = '' },
-    change = { text = '' },
-    delete = { text = '' },
-    topdelete = { text = '' },
-    changedelete = { text = '' },
-  },
-  on_attach = function(bufnr)
-    vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk,
-      { buffer = bufnr, desc = 'Preview git hunk' })
-
-    -- don't override the built-in and fugitive keymaps
-    local gs = package.loaded.gitsigns
-    vim.keymap.set({ 'n', 'v' }, ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
-    vim.keymap.set({ 'n', 'v' }, '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
-  end,
-}
-
-
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
 
 require('Fuzzel.lsp')
 require('Fuzzel.mappings')
@@ -64,22 +12,10 @@ require('Fuzzel.settings')
 require('Fuzzel.telescope')
 require('Fuzzel.treesitter')
 require('Fuzzel.undotree')
+require('Fuzzel.theme')
 
 vim.keymap.set('n', '<leader>gr', ':r !tr -dc a-z0-9 < /dev/urandom | head -c 36;echo<CR>',
 { desc = "Generate Random UID" })
-
--- Colorscheme
-require('catppuccin').setup({
-  flavour = 'frappe',
-  transparent_background = false,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    telescope = true,
-    treesitter = true,
-  },
-})
-vim.cmd('colorscheme catppuccin')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/Fuzzel/theme.lua
@@ -0,0 +1,81 @@
+local colorizer = require 'colorizer'
+local comment = require 'Comment'
+local gitsigns = require 'gitsigns'
+local noice = require 'noice'
+local notify = require 'notify'
+local todo_comments = require 'todo-comments'
+local lualine = require 'lualine'
+local catppuccin = require 'catppuccin'
+
+colorizer.setup()
+comment.setup()
+todo_comments.setup()
+noice.setup()
+notify.setup()
+
+lualine.setup {
+    options = {
+        icons_enabled = false,
+        theme = 'catppuccin',
+        component_separators = { left = '', right = '' },
+        section_separators = {
+            left = '',
+            right = '',
+        }
+    },
+}
+
+gitsigns.setup {
+  signs = {
+    add = { text = '' },
+    change = { text = '' },
+    delete = { text = '' },
+    topdelete = { text = '' },
+    changedelete = { text = '' },
+  },
+  on_attach = function(bufnr)
+    vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk,
+      { buffer = bufnr, desc = 'Preview git hunk' })
+
+    -- don't override the built-in and fugitive keymaps
+    local gs = package.loaded.gitsigns
+    vim.keymap.set({ 'n', 'v' }, ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+    vim.keymap.set({ 'n', 'v' }, '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+  end,
+}
+
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- Colorscheme
+catppuccin.setup({
+  flavour = 'frappe',
+  transparent_background = false,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    telescope = true,
+    treesitter = true,
+  },
+})
+vim.cmd('colorscheme catppuccin')
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
