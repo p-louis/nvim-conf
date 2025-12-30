6ba6dc4327d01cfed0e6eb2e1481ac02032c45f4
Patrick Louis <patrick.louis@linked-planet.com>
2025-12-30 11:38:59 +0100

    lsp: enable auto-formatting

--- nvim/lua/Fuzzel/lsp.lua
+++ nvim/lua/Fuzzel/lsp.lua
@@ -23,7 +23,6 @@ local on_attach = function(client, bufnr)
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-  nmap('<leader>lf', vim.lsp.buf.format(), '[L]sp [F]ormat')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -144,6 +143,41 @@ for server, server_config in pairs(language_servers) do
   end
 
   lspconfig[server].setup(config)
+
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+      if not client then return end
+      if client:supports_method('textDocument/implementation') then
+        -- Create a keymap for vim.lsp.buf.implementation ...
+        vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, { desc = '[L]sp [I]mplementation' })
+      end
+
+      -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+      if client:supports_method('textDocument/completion') then
+        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+        -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+        -- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+        vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+      end
+
+      -- Auto-format ("lint") on save.
+      -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+      if not client:supports_method('textDocument/willSaveWaitUntil')
+          and client:supports_method('textDocument/formatting') then
+        vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, { desc = '[L]sp [F]ormat' })
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          end,
+        })
+      end
+    end,
+  })
 end
 
 -- Setup neovim lua configuration
