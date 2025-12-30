db4ef82671c01efc755ac82ed82b15cc29e4c28c
Patrick Louis <patrick.louis@linked-planet.com>
2025-12-30 11:25:26 +0100

    lsp: add binding for formatting

--- nvim/lua/Fuzzel/lsp.lua
+++ nvim/lua/Fuzzel/lsp.lua
@@ -23,6 +23,8 @@ local on_attach = function(client, bufnr)
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
+  nmap('<leader>lf', vim.lsp.buf.format(), '[L]sp [F]ormat')
+
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
